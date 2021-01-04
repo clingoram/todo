14e53ec8e9238899ec9d43b2417eb920fca58c42
Mavis <llingchen.27@gmail.com>
2021-01-04 10:11:26 +0800

    add Vue component to blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+        
+        <title>Laravel-ToDo List</title>
+    </head>
+    <body>
+      <div id="app">
+          <index-component></index-component>
+      </div>
+      <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    </body>
+</html>
