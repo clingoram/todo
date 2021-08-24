a92edb7b6e62804edddc2d295b41b86f83ebb1bc
Mavis <llingchen.27@gmail.com>
2021-08-24 16:06:51 +0800

    edit vue

--- resources/js/components/Calendar.vue
+++ resources/js/components/Calendar.vue
@@ -1,38 +1,99 @@
 <template>
-  <b-button v-b-modal.my-modal>Show Modal</b-button>
-  <!-- <b-row>
-    <b-col md="auto">
-      <b-calendar
-        v-model="value"
-        @context="onContext"
-        locale="en-US"
-      ></b-calendar>
-    </b-col>
-    <b-col>
-      <p>
-        Value: <b>'{{ value }}'</b>
-      </p>
-      <p class="mb-0">Context:</p>
-      <pre class="small">{{ context }}</pre>
-    </b-col>
-  </b-row> -->
-</template>
+  <div>
+    <!-- <b-button v-b-modal.modal-1>新增代辦事項</b-button>
 
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal> -->
+    <b-row>
+      <b-col md="auto">
+        <b-calendar
+          v-model="value"
+          @context="onContext"
+          block
+          locale="en-US"
+          v-b-modal.modal-prevent-closing
+        ></b-calendar>
+      </b-col>
+
+      <b-modal
+        id="modal-prevent-closing"
+        ref="modal"
+        title="新增代辦事項"
+        @show="resetModal"
+        @hidden="resetModal"
+        @ok="handleOk"
+      >
+        <form ref="form" @submit.stop.prevent="handleSubmit">
+          <b-form-group
+            label="事項:"
+            label-for="name-input"
+            invalid-feedback="Name is required"
+            :state="nameState"
+          >
+            <b-form-input
+              id="name-input"
+              v-model="name"
+              :state="nameState"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </form>
+      </b-modal>
+    </b-row>
+    <!-- <modal-info></modal-info> -->
+  </div>
+</template>
 <script>
+import Modalinfo from "./MoreInfo";
 export default {
   mounted() {
     console.log("calendar");
   },
-  // data() {
-  //   return {
-  //     value: "",
-  //     context: null,
-  //   };
-  // },
-  // methods: {
-  //   onContext(ctx) {
-  //     this.context = ctx;
-  //   },
-  // },
+  components: {
+    Modalinfo,
+  },
+  props: ["dateValue"],
+  data() {
+    return {
+      value: "",
+      context: null,
+      name: "",
+      nameState: null,
+      submittedNames: [],
+    };
+  },
+  methods: {
+    onContext(ctx) {
+      this.context = ctx;
+    },
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid;
+      return valid;
+    },
+    resetModal() {
+      this.name = "";
+      this.nameState = null;
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
+      }
+      // Push the name to submitted names
+      this.submittedNames.push(this.name);
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide("modal-prevent-closing");
+      });
+    },
+  },
 };
 </script>
--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,53 +1,20 @@
 <template>
-  <div id="todo_list">
-    <b-button v-b-modal.my-modal>Show Modal</b-button>
+  <div id="area">
     <h1>ToDo List</h1>
     <span>Get things done!!!</span>
     <show-calendar></show-calendar>
-    <add-task v-on:reloadlist="getListdata()"> </add-task>
-
-    <task-view
-      v-bind:tasks="item_data"
-      v-on:reloadlist="getListdata()"
-    ></task-view>
   </div>
 </template>
 <script>
 // import components
-// add task form
-import AddTask from "./AddTask";
-// lists of task
-import TaskView from "./TaskView";
 import ShowCalendar from "./Calendar";
 
 export default {
   mounted() {
-    console.log("hey");
-  },
-  created() {
-    this.getListdata();
+    console.log("index");
   },
   components: {
-    AddTask,
-    TaskView,
     ShowCalendar,
-  },
-  data: function () {
-    return {
-      item_data: [],
-    };
-  },
-  methods: {
-    getListdata() {
-      axios
-        .get("api/item")
-        .then((response) => {
-          this.item_data = response.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
   },
 };
 </script>
@@ -64,17 +31,8 @@ body {
   font-family: sans-serif;
   height: 100%;
 }
-#todo_list {
-  margin: 4rem auto;
-  padding: 2rem 3rem 3rem;
-  max-width: 500px;
-  background: #ff6666;
-  color: #000000;
-}
-#todo_list h1 {
-  font-weight: normal;
-  font-size: 2.6rem;
-  letter-spacing: 0.05em;
-  border-bottom: 1px solid #e5e5e5;
+#area {
+  padding: 80px;
+  width: 100%;
 }
 </style>
